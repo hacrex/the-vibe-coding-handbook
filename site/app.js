2acc254c869e7e1387560e1d3facf154879cda82
IT Support <kashvit@live.in>
2026-07-25 17:21:09 +0530

    Redirect modal lesson links to catalog page instead of GitHub

--- site/app.js
+++ site/app.js
@@ -285,11 +285,7 @@
 
       html += '<div class="modal-lesson' + (userComplete ? ' user-done' : '') + '">';
       html += '<span class="modal-lesson-status ' + statusClass + '" role="img" aria-label="' + (userComplete ? 'completed' : statusClass.replace(/-/g, ' ')) + '"' + (userComplete ? ' title="You completed this lesson"' : '') + '></span>';
-      if (l.url) {
-        html += '<a href="' + l.url + '">' + escapeHtml(l.name) + '</a>';
-      } else {
-        html += '<a>' + escapeHtml(l.name) + '</a>';
-      }
+      html += '<a href="catalog.html?phase=' + p.id + '">' + escapeHtml(l.name) + '</a>';
       html += '<span class="modal-lesson-type" data-type="' + escapeHtml(l.type) + '"' + (l.combines ? ' title="Combines: ' + escapeHtml(l.combines) + '"' : '') + '>' + escapeHtml(l.type) + '</span>';
       html += '<span class="modal-lesson-lang">' + escapeHtml(l.lang) + '</span>';
 
